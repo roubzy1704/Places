53f217341625b79879d4989c2b878a221f4edfb3
roubzy1704 <roubzy1704@yahoo.com>
2020-11-09 13:21:22 -0600

    Updates

---
+++ precache-manifest.35dfab3a8ba70c54f493a2f2a204c572.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e11365805920b98872b69181299de8",
+    "url": "/Places/index.html"
+  },
+  {
+    "revision": "0039bb3c0fc377ffa439",
+    "url": "/Places/static/css/4.f75c0ada.chunk.css"
+  },
+  {
+    "revision": "756301b5831982e5b69b",
+    "url": "/Places/static/css/5.d7da4067.chunk.css"
+  },
+  {
+    "revision": "4b895fef378d55960417",
+    "url": "/Places/static/css/6.be4ef35a.chunk.css"
+  },
+  {
+    "revision": "c09741d06ac7c73bfa9a",
+    "url": "/Places/static/css/7.c62022e3.chunk.css"
+  },
+  {
+    "revision": "d16ca27d214791fe5c11",
+    "url": "/Places/static/css/8.028a6ed4.chunk.css"
+  },
+  {
+    "revision": "ffb28464b1efc7592c9f",
+    "url": "/Places/static/css/main.f49499bf.chunk.css"
+  },
+  {
+    "revision": "340920cbd8b909706656",
+    "url": "/Places/static/js/0.5ddde6ec.chunk.js"
+  },
+  {
+    "revision": "ddcf0021c3b1f78e960d",
+    "url": "/Places/static/js/3.7f6a6a6a.chunk.js"
+  },
+  {
+    "revision": "0039bb3c0fc377ffa439",
+    "url": "/Places/static/js/4.03dda076.chunk.js"
+  },
+  {
+    "revision": "756301b5831982e5b69b",
+    "url": "/Places/static/js/5.cd26d12a.chunk.js"
+  },
+  {
+    "revision": "4b895fef378d55960417",
+    "url": "/Places/static/js/6.abdc1f2b.chunk.js"
+  },
+  {
+    "revision": "c09741d06ac7c73bfa9a",
+    "url": "/Places/static/js/7.d85d5ade.chunk.js"
+  },
+  {
+    "revision": "d16ca27d214791fe5c11",
+    "url": "/Places/static/js/8.59f30c93.chunk.js"
+  },
+  {
+    "revision": "ffb28464b1efc7592c9f",
+    "url": "/Places/static/js/main.ec501b35.chunk.js"
+  },
+  {
+    "revision": "cd3c4874a3b8ddd1202c",
+    "url": "/Places/static/js/runtime-main.db63ed44.js"
+  }
+]);
